350bb65b3a52b7bbc4bceaee5df2bc2d8356cb28
pabcrudel <pabcrudel@gmail.com>
2023-12-09 17:00:19 +0100

    add: Disclaimer

--- test/react.test.jsx
+++ test/react.test.jsx
@@ -1,5 +1,12 @@
 import { describe, it } from 'vitest'
 
+/**
+ * DISCLAIMER: (December 2023)
+ * That's my first time using React. I so confident with Vue.js so I consider
+ * that I can face this challenge. However, I will copy-paste from the
+ * tutorial all the source code that I haven't seen before.
+ */
+
 describe('Calculator', () => {
   it('', () => {})
 })
